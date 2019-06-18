2c9fa6ba1441f96847ab116659246071a79524d3
Alvin Kingman Gao <akgao@uci.edu>
2019-06-17 21:15:48 -0700

    Solutions for 2

---
+++ Python3/2.Add_Two_Sum.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        temp = sentinal = ListNode(0)
+        carry = 0
+        while(l1 or l2 or carry):
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            temp.next = temp = ListNode(carry % 10)
+            carry = carry // 10
+        return sentinal.next
