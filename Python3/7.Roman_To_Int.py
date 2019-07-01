bac8db8aea4ab4764353238363ec2f0872469a97
Alvin Kingman Gao <akgao@uci.edu>
2019-07-01 13:21:05 -0700

    Solutions for 7

--- Python3/7.Roman_To_Int.py
+++ Python3/7.Roman_To_Int.py
@@ -1,17 +1,4 @@
 class Solution:
-    def is_next_bigger(self, s: str) -> int:
-        mappings = {
-            "IV": 4,
-            "IX": 9,
-            "XL": 40,
-            "XC": 90,
-            "CD": 400,
-            "CM": 900,
-        }
-        if s not in mappings.keys():
-            return 0
-        return mappings[s]
-
     def romanToInt(self, s: str) -> int:
         mappings = {
             "I": 1,
@@ -22,18 +9,10 @@ class Solution:
             "D": 500,
             "M": 1000,
         }
-        i = total = 0
-
-        while i < len(s):
-            if i + 1 != len(s):
-                num = self.is_next_bigger(s[i:i+2])
-                if num:
-                    total += num
-                    i += 1
-                else:
-                    total += mappings[s[i]]
+        total = 0
+        for i in range(len(s) - 1):
+            if mappings[s[i+1]] > mappings[s[i]]:
+                total -= mappings[s[i]]
             else:
                 total += mappings[s[i]]
-            i += 1
-
-        return total
+        return total + mappings[s[-1]]
